b0b845cbe351a8d0df9b67c896f0ad36038775c4
John Lazzarini <j.lazzarini.work@gmail.com>
2016-09-07 21:42:50 -0700

    Update Automata README.java

--- Automata README.java
+++ Automata README.java
@@ -1,5 +1,5 @@
 This project was done as part of a course on software engineering -- the brief "main"
-method was provided to me by an instructor, but the core mechanics and algorithms are by my own
-design.  Since the functionality of this program is non trivial, I have
+method was provided to me by an instructor, but the core mechanics and algorithms are of my own
+design and development.  Since the functionality of this program is non trivial, I have
 provided more documentation than I normally would to explain various
 method features and purposes.
